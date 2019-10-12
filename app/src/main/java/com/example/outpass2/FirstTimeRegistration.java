d1c616a741e2f17f7df798798b6688b8247541b4
BabesGotByte <anshulagarwaliiita@gmail.com>
2019-10-12 20:18:38 +0530

    signup class constraints and firsttimeregistration class added

---
+++ app/src/main/java/com/example/outpass2/FirstTimeRegistration.java
@@ -0,0 +1,63 @@
+package com.example.outpass2;
+
+public class FirstTimeRegistration {
+
+    private String name,contact,room,hostel,gname,gnumber;
+
+    public FirstTimeRegistration(String name, String contact, String room, String hostel, String gname, String gnumber) {
+        this.name = name;
+        this.contact = contact;
+        this.room = room;
+        this.hostel = hostel;
+        this.gname = gname;
+        this.gnumber = gnumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public void setHostel(String hostel) {
+        this.hostel = hostel;
+    }
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+
+    public void setGnumber(String gnumber) {
+        this.gnumber = gnumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public String getHostel() {
+        return hostel;
+    }
+
+    public String getGname() {
+        return gname;
+    }
+
+    public String getGnumber() {
+        return gnumber;
+    }
+}
--- app/src/main/java/com/example/outpass2/signup.java
+++ app/src/main/java/com/example/outpass2/signup.java
@@ -3,29 +3,62 @@ package com.example.outpass2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
-
-    Button b1;
+    private EditText name_t,contact_t,room_t,hostel_t,gname_t,gnumber_t;
+    private Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         b1=findViewById(R.id.but);
+        name_t=findViewById(R.id.name);
+        contact_t=findViewById(R.id.contact);
+        room_t=findViewById(R.id.room);
+        hostel_t=findViewById(R.id.hostel);
+        gname_t=findViewById(R.id.guardian_name);
+        gnumber_t=findViewById(R.id.guardian_contact);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name_e = name_t.getText().toString().trim();
+                String contact_e = contact_t.getText().toString();
+                String room_e = room_t.getText().toString().trim();
+                String hostel_e = hostel_t.getText().toString();
+                String gname_e = gname_t.getText().toString().trim();
+                String gnumber_e = gnumber_t.getText().toString();
 
-                if(photosStorage.photos!=null){
-                    if(photosStorage.photos.size()>=1) {
-                        finish();
-                        startActivity(new Intent(signup.this, navbar.class));
-                    }
 
+                if (TextUtils.isEmpty(name_e)) {
+                    name_t.setError("This field should not be empty");
+                    return;
                 }
-                startActivity(new Intent(signup.this, photo.class));
+                if (TextUtils.isEmpty(contact_e)) {
+                    contact_t.setError("This field should not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(room_e)) {
+                    room_t.setError("This field should not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(hostel_e)) {
+                    hostel_t.setError("This field should not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(gname_e)) {
+                    gname_t.setError("This field should not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(gnumber_e)) {
+                    gnumber_t.setError("This field should not be empty");
+                    return;
+                }
+
+                startActivity(new Intent(signup.this, navbar.class));
 
             }
         });
