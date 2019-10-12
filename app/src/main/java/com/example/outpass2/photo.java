c216b9d08555ad5243e6f09e2448cc943b69b780
BabesGotByte <anshulagarwaliiita@gmail.com>
2019-10-12 14:27:31 +0530

    camera features added

--- app/src/main/java/com/example/outpass2/photo.java
+++ app/src/main/java/com/example/outpass2/photo.java
@@ -1,13 +1,70 @@
 package com.example.outpass2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class photo extends AppCompatActivity {
+
+    ArrayList<File> photos=new ArrayList<>();
+    File photoFile = null;
+    String currentPhotoPath;
+    ImageView i1;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        for(int i=0;i<3;i++){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 0);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                try {
+                    photoFile = createImageFile();
+                    photos.add(photoFile);
+
+                }
+                catch (IOException ex) {
+
+                }
+            }
+        }
+
+
+//        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+//            super.onActivityResult(requestCode, resultCode, data);
+//            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+//            //imageview.setImageBitmap(bitmap);
+//        }
+
     }
 }
